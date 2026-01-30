a2714c291968837b4437653fe7cceeae76f8819d
Carlos Bernal <carlosbernal4409@gmail.com>
2026-01-30 15:02:50 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,3 @@
 * Copyright 2013-2023 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
 */
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
